32aaade56826ab465bca18bc615de10b5949ec4a
Brandon Ly <brandonly@live.com>
2014-12-05 18:30:23 -0800

    Created the initial file.

---
+++ alphaBlending.js
@@ -0,0 +1,16 @@
+/*
+	Functions and variabes etc for Alpha Blending!
+
+	Main Author: Brandon Ly
+
+*/
+
+function initAlphaBlending()
+{
+	gl.enable(gl.BLEND);
+}
+
+function renderAlphaBlending()
+{
+
+}
